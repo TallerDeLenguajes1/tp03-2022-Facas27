8f94b0002c21ebcb4998e8b7e2700cc98f58fd9a
Facundo Abbas <facundoabbas@hotmail.com>
2022-04-19 18:58:37 -0300

    Fix commit

--- tp3_3.c
+++ tp3_3.c
@@ -8,55 +8,84 @@ struct Producto {
     int Cantidad; // entre 1 y 10
     char *TipoProducto; // Algún valor del arreglo TiposProductos
     float PrecioUnitario; // entre 10 - 100
-};
+}typedef Producto;
 struct Cliente {
         int ClienteID; // Numerado en el ciclo iterativo
         char *NombreCliente; // Ingresado por usuario
         int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
-        Producto *Productos //El tamaño de este arreglo depende de la variable
+        struct Producto *Productos //El tamaño de este arreglo depende de la variable
         // “CantidadProductosAPedir”
-};
-int CalcularPF(struct Producto *p);
+}typedef Cliente;
+float CalcularPF(Producto p);
 
 
 
 int main(){
-    int cant, totalFN= 0;
+    int cant;
+    float totalFN = 0;
     srand(time(NULL));
 
     printf("Introduzca la cantidad de clientes \n");
     scanf("%i",&cant);
-   struct Cliente * clientes = malloc(cant * sizeof(struct Cliente));
+    Cliente *clientes;
+    clientes = (Cliente *) malloc(cant * sizeof(Cliente));
     for (int  i = 0; i < cant; i++)
     {
-        clientes[i]->ClienteID = i;
-        clientes[i]->NombreCliente = (char *) malloc(100*sizeof(char));
+        clientes[i].ClienteID = i;
+        fflush(stdin);
+        clientes[i].NombreCliente = (char *) malloc(100*sizeof(char));
         printf("Ingrese el nombre del cliente \n");
-        gets(clientes[i]->NombreCliente);
-        clientes[i]->CantidadProductuosAPedir = rand() % 4 + 1;
-        clientes[i]->Productos =  malloc (clientes[i]->CantidadProductuosAPedir * sizeof(struct Producto));
-        for (int z = 0; z < clientes[i]->CantidadProductuosAPedir; z++)
+        gets(clientes[i].NombreCliente);
+        clientes[i].CantidadProductosAPedir = rand() % 4 + 1;
+        clientes[i].Productos = (Producto *) malloc (clientes[i].CantidadProductosAPedir * sizeof(Producto));
+        for (int z = 0; z < clientes[i].CantidadProductosAPedir; z++)
         {
-           Productos[z]->ProductoID = z;
-           Productos[z]->Cantidad = rand() % 9 + 1 ;
+            float total = 0;
+            char pro;
+            pro =(char *)malloc(100);
+           clientes[i].Productos[z].ProductoID = z;
+           clientes[i].Productos[z].Cantidad = rand() % 9 + 1 ;
            int ran = rand()%6;
-           char *pro[100];
            strcpy(pro, TiposProductos[ran]);
-           Productos[z]->TipoProducto =  malloc(strlen(pro) * sizeof * Productos[z]->TipoProducto );
-           strcpy(Productos[z]->TipoProducto , pro);
-           Productos[z]->PrecioUnitario = rand() % 90 + 10;
-           int total;
-           total = CalcularPF(Productos[z])
-           printf("El total a pagar por el producto %i es %i: \n"z+1 , total));
+           clientes[i].Productos[z].TipoProducto =  (char * )malloc(strlen(pro)+1 );
+           strcpy(clientes[i].Productos[z].TipoProducto , pro);
+           clientes[i].Productos[z].PrecioUnitario = rand() % 90 + 10;
+           total = CalcularPF(clientes[i].Productos[z]);
+           printf("El total a pagar por el producto %i es %i: \n", z+1 , total);
            totalFN  += total;
 
 
         }
         
-
-
-
+        
     }
+        printf("Mostrando los datos: \n");
+        for (int i = 0; i < cant; i++)
+        {
+              printf("Cliente %d\n", i + 1);
+              printf("ID del cliente : %d /n", clientes[i].ClienteID);
+              puts(clientes[i].NombreCliente);
+              printf("\nCantidad de Productos: %d\n",clientes[i].CantidadProductosAPedir);
+              printf("\nProductos solicitados: \n");
+              for (int z = 0; z < clientes[i].CantidadProductosAPedir; z++)
+              {
+                  puts(clientes[i].Productos[z].TipoProducto);
+                  printf("Precio Unitario : %2f /n", clientes[i].Productos[z].PrecioUnitario);
+                  printf("Cantidad : %d /n", clientes[i].Productos[z].Cantidad);
+                  printf("Subtotal : %2f /n", CalcularPF(clientes[i].Productos[z]));
+
+              }
+              printf("Precio a pagar en total por el cliente: %2f /n", totalFN);
+        }
+
+        for(int i=0;i<cant;i++){
+        for(int z=0;z<clientes[i].CantidadProductosAPedir;z++){
+            free(clientes[i].Productos[z].TipoProducto);
+            }
+            free(clientes[i].Productos);
+            free(clientes[i].NombreCliente);
+        }
+       free(clientes);
     
 
 
@@ -73,9 +102,9 @@ int main(){
 
     return  0;
 }
-int CalcularPF(struct Producto *p){
-    int total;
-    total = (*p->Cantidad) * (*p->PrecioUnitario) ;
+float CalcularPF( Producto p){
+    float total;
+    total = (p.Cantidad) * (p.PrecioUnitario) ;
     return total;
 
 
--- tp3_4.c
+++ tp3_4.c
@@ -3,44 +3,26 @@
 #include <string.h>
 
 int main(){
-     char *p1, *p2, *p3 , *p4 , *p5;
-     p1= (char *) malloc(100*sizeof(char));
-     p2= (char *) malloc(100*sizeof(char));
-     p3= (char *) malloc(100*sizeof(char));
-     p4= (char *) malloc(100*sizeof(char));
-     p5= (char *) malloc(100*sizeof(char));
-     printf("Ingrese el primer nombre \n");
-     gets(p1);
-     printf("Ingrese el segundo nombre \n");
-     gets(p2);
-     printf("Ingrese el tercero nombre \n");
-     gets(p3);
-     printf("Ingrese el cuarto nombre \n");
-     gets(p4);
-     printf("Ingrese el quinto nombre \n");
-     gets(p5);
-        char *V [5] = {p1, p2, p3, p4, p5};
-        for (int i = 0; i < 5; i++)
-        {
-            puts(V[i]);
-        }
-        free(p1);
-        free(p2);
-        free(p3);
-        free(p4);
-        free(p5);
-
-        
-
-
-
-
-
-
-
-
-
-
+    int cantidad;
+    char **V;
+    printf("Ingrese la cantidad de nombres a ingresar: ");
+    scanf("%d",&cantidad);
+    fflush(stdin);
+    V = (char **) malloc(cantidad * sizeof(char *));
+    for(int i=0;i<cantidad;i++){
+        fflush(stdin);
+        V[i]=(char *) malloc(100);
+        printf("Ingrese el nombre %d: ",i+1);
+        gets(V[i]); 
+    }
+    printf("Nombres: ");
+    for(int i=0;i<cantidad;i++){
+        puts(V[i]);
+    }
+     for(int i=0;i<cantidad;i++){
+        free(V[i]);
+    }
+    free(V);
 
     return 0;
 }
